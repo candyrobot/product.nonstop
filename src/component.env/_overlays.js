7bb9f123d252610877bdb61e6d8010e6aa8b6c60
田中 秀明 <tanaka_hideaki@aja-kk.co.jp>
2018-12-12 17:40:59 +0900

    update

--- src/component.env/_overlays.js
+++ src/component.env/_overlays.js
@@ -1,7 +1,10 @@
 import $ from 'jquery';
+import {
+  isAndroid
+} from './_util';
 import './overlay.css';
 export default `
-<div id="component-login" class="component-overlay" style="display: none;">
+<div id="component-login" class="component-overlay flex-justify-center" style="display: none;">
   <div class="small">忘れないようにメモを。</div>
   <input type="text" class="email" name="email" placeholder="email">
   <input type="password" class="password" name="password" placeholder="password">
@@ -22,7 +25,7 @@ export default `
   <div class="close" onclick="$(this).parent().hide(300)">×</div>
 </div>
 
-<div id="component-logout" class="component-overlay" style="display: none;">
+<div id="component-logout" class="component-overlay flex-justify-center" style="display: none;">
   <h1>＼(^o^)／</h1>
   <h5>xxxxxxxxxx@xxxxx.xxx</h5>
   <button onclick="logout()">ログアウト</button>
@@ -30,9 +33,15 @@ export default `
 </div>
 
 <div id="webview" class="component-overlay" style="display: none;">
+  <div class="component-mediabox">
+    <img src="https://nonstop-vr.firebaseapp.com//icons.nonstop/icon-384x384.png">
+    <h4 class="title">やり方を動画で解説</h4>
+    <div class="desc">
+      ${isAndroid() ? 'Chrome' : 'Safari'}でホーム画面に
+      <br>追加できます。
+    </div>
+  </div>
   <div class="frombottom">
-    <p>やり方を動画で解説</p>
-
     <iframe frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
     <!-- <iframe src="" frameborder="0"></iframe> -->
