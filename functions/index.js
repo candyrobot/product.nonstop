9db771e9399b01e8f83ca31b4b3c91606b8ed221
田中 秀明 <tanaka_hideaki@aja-kk.co.jp>
2018-12-26 20:02:28 +0900

    SNAPSHOT

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,11 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.writeUrl = functions.storage.object().onFinalize((object) => {
-  const bucketName = 'hoge.appspot.com'; // ご自身の
   const filePath = object.name;
   const db = admin.firestore();
 
   db.collection('images').add({
     filePath,
-    downloadUrl: `https://firebasestorage.googleapis.com/v0/b/${bucketName}/o/${encodeURIComponent(filePath)}?alt=media`,
   })
   .then(() => console.log('Done'))
   .catch((e)=> console.log(e)); 
