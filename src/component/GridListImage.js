ea28c9aceca392a5d1180fc067f2ef7f66874e9f
Hideaki Tiánzhōng <rivalknockout@gmail.com>
2019-01-02 15:22:24 +0900

    add transition to /src/component/GridListImage.js

--- src/component/GridListImage.js
+++ src/component/GridListImage.js
@@ -4,6 +4,7 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import Grow from '@material-ui/core/Grow';
 import Favorite from '../model/Favorite';
 
 export default class extends Component {
@@ -15,40 +16,46 @@ export default class extends Component {
     <GridList
       id="component-images" className="gridList"
       cellHeight='auto' spacing={1}>
+      {/*INFO: https://material-ui.com/utils/transitions/*/}
       {this.props.images.map(tile => (
-        <GridListTile
-          className="Pic fas fa-unlink"
-          testAttr={tile.id}
-          key={tile.id} cols={tile.featured ? 2 : 1} rows={tile.featured ? 2 : 1}>
-          <div
-            className="background"
-            data-load-image={tile.url}
-            onClick={()=> window.Route.push('images', { id: tile.id }).refresh()}
+        <Grow in={true}
+          style={{ transformOrigin: '0 0 0' }}
+        >
+            {/*ref={(node)=> $(node).inView().length}*/}
+          <GridListTile
+            className="Pic fas fa-unlink"
+            testAttr={tile.id}
+            key={tile.id} cols={tile.featured ? 2 : 1} rows={tile.featured ? 2 : 1}>
+            <div
+              className="background"
+              data-load-image={tile.url}
+              onClick={()=> window.Route.push('images', { id: tile.id }).refresh()}
             >
             </div>
-          {/*<img src={tile.url} alt={tile.title} />*/}
-          <GridListTileBar
-            style={{ background: 'inherit', pointerEvents: 'none' }}
-            title={tile.title}
-            titlePosition="bottom"
-            actionIcon={
-              !! window.dat.favorites.where({imageID: tile.id, userID: window.dat.session.id}).length
-              ?
-              <IconButton
-                style={{ pointerEvents: 'initial', color: 'rgba(255, 0, 0, 0.8)' }}
-                onClick={()=> Favorite.delete(tile.id)}>
-                <FavoriteIcon />
-              </IconButton>
-              :
-              <IconButton
-                style={{ pointerEvents: 'initial' }}
-                onClick={()=> Favorite.create(tile.id)}>
-                <FavoriteIcon />
-              </IconButton>
-            }
-            actionPosition="right"
-          />
-        </GridListTile>
+            {/*<img src={tile.url} alt={tile.title} />*/}
+            <GridListTileBar
+              style={{ background: 'inherit', pointerEvents: 'none' }}
+              title={tile.title}
+              titlePosition="bottom"
+              actionIcon={
+                !! window.dat.favorites.where({imageID: tile.id, userID: window.dat.session.id}).length
+                ?
+                <IconButton
+                  style={{ pointerEvents: 'initial', color: 'rgba(255, 0, 0, 0.8)' }}
+                  onClick={()=> Favorite.delete(tile.id)}>
+                  <FavoriteIcon />
+                </IconButton>
+                :
+                <IconButton
+                  style={{ pointerEvents: 'initial' }}
+                  onClick={()=> Favorite.create(tile.id)}>
+                  <FavoriteIcon />
+                </IconButton>
+              }
+              actionPosition="right"
+            />
+          </GridListTile>
+        </Grow>
       ))}
     </GridList>
     );
