e801046d034194477503c59daf9efaa567841cf9
Hideaki Tiánzhōng <rivalknockout@gmail.com>
2019-01-02 15:21:45 +0900

    rewrite PaperUser.js to react

--- src/component.env/PaperUser.js
+++ src/component.env/PaperUser.js
@@ -1,63 +1,62 @@
+import React, { Component } from 'react';
 import './PaperUser.css';
+import Route from './Route';
 
-export default class PaperUser {
-  html(user) {
-    if(window.dat.favorites.where({ userID: user.id }).length === 0)
-      return '';
-    var n = window.dat.favorites.where({ userID: user.id }).length - 3;
-    return `
-    <div class="PaperUser">
-      <div class="user">
-        <i class="fas fa-user-circle"></i>
+// TODO: 他... の部分を黒グラデ白文字にしたい
+
+export default class extends Component {
+  render() {
+    const user = this.props.user;
+    const favorites = window.dat.favorites.where({ userID: user.id });
+    if(favorites.length === 0)
+      return null;
+    return (
+    <div className="PaperUser">
+      <div className="user">
+        <i className="fas fa-user-circle"></i>
         <div
-          style="padding-left: 0.35em;"
+          style={{ paddingLeft: '0.35em' }}
           >
-          <div
-            style="
-            font-size: 10px;
-            "
-            >
-            ${user.created_at.replace('T', ' ').replace(/\..*$/, '')}
+          <div style={{ fontSize: 10 }}>
+            {user.created_at.replace('T', ' ').replace(/\..*$/, '')}
           </div>
           <div
-            style="
-            font-size: 11px;
-            font-weight: bold;
-            "
-            >
+            style={{
+              fontSize: 11,
+              fontWeight: 'bold'
+            }}
+          >
             ？？？？　このユーザーのお気入り▼
           </div>
         </div>
       </div>
       <div>
-        ${window.dat.favorites.where({ userID: user.id }).reduce((prev, f, i)=> {
-          if(i > 2)
-            return prev;
+        {/*.splice(0, 5)*/}
+        {window.dat.favorites.where({ userID: user.id })
+        .map((f, i)=> {
           const image = window.dat.images.find(f.imageID);
-          return prev + `
+          return (
           <div
-            class="image"
-            onclick="Route.push('images', { id: ${image.id} }).refresh()"
-            style="
-            background-image: url(${image.url})
-            "
+            key={i}
+            className="image"
+            onClick={()=> Route.push('images', { id: image.id }).refresh()}
+            style={{ backgroundImage: `url(${image.url})` }}
             >
           </div>
-          `
-        }, '') || '<p align="center">なし</p>'}
+          )
+        })}
 
         <div
-          class="image"
-          onclick="xxxxxxx"
-          style="height: initial"
+          className="image"
+          style={{ height: 'initial' }}
           >
-          <h2 style="margin-bottom: -10px">
-            …他${n > 0 ? n : 2}件！
-          </h2>
-          <p class="small">（全部見るには登録する必要があります）</p>
+          <h3 style={{ marginBottom: -10 }}>
+            …他{favorites.length}件
+          </h3>
+          <p className="small">（全部見るには登録する必要があります）</p>
         </div>
       </div>
     </div>
-    `;
+    );
   }
 }
