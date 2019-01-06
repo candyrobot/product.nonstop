c3a4ca931cdfeb9f7130ce9cf7f6ce67d85c1ebc
Hideaki Tiánzhōng <rivalknockout@gmail.com>
2019-01-07 01:36:54 +0900

    update to show the banner with no react-error.

--- src/component/GridListImage.js
+++ src/component/GridListImage.js
@@ -9,9 +9,34 @@ import {
 import GridListTileImage from '../component.env/GridListTileImage';
 import '../component.env/GridList.css';
 
+class Banner extends Component {
+  render() {
+    return (
+    <div
+      style={{ padding: undefined, width: undefined, height: undefined }}
+      className="message"
+      onClick={()=> {
+        if (isAndroid()) {
+          return showWebview('https://www.youtube.com/embed/f9MsSWxJXhc');
+        } else {
+          return showWebview('https://www.youtube.com/embed/8iueP5sRQ-Y');
+        }
+      }}
+    >
+      スマホのホーム画面にこのアプリを追加することができるのです
+      <i>(ここをタップ)</i>
+    </div>
+    );
+  }
+}
+
 export default class extends Component {
   render() {
-    const images = this.props.images;
+    const images = this.props.images.reduce((a, image, i)=> {
+      i === 0 || i % 12 || a.push({ type: 'banner' });
+      a.push(image);
+      return a;
+    }, []);
 
     if (!images)
       return null;
@@ -20,23 +45,11 @@ export default class extends Component {
     <GridList
       id="component-images"
       cellHeight='auto' spacing={1}>
-      {images.map((image, i) => [
-        i % 12 || <div
-          style={{ padding: undefined, width: undefined, height: undefined }}
-          className="message"
-          onClick={()=> {
-            if (isAndroid()) {
-              return showWebview('https://www.youtube.com/embed/f9MsSWxJXhc');
-            } else {
-              return showWebview('https://www.youtube.com/embed/8iueP5sRQ-Y');
-            }
-          }}
-        >
-          スマホのホーム画面にこのアプリを追加することができるのです
-          <i>(ここをタップ)</i>
-        </div>,
+      {images.map((image, i) => (
+        image.type === 'banner' ?
+        <Banner key={i} /> :
         <GridListTileImage key={i} image={image} />
-      ])}
+      ))}
     </GridList>
     );
   }
