8faf13e6a6e464cfae358481c49283968fd4f7d6
Hideaki Tiánzhōng <rivalknockout@gmail.com>
2019-02-15 10:30:27 +0900

    fix

--- src/model/Image.js
+++ src/model/Image.js
@@ -19,7 +19,7 @@ export default window.Image = new class {
 	// }
 
 	sortByRelated(imageID) {
-		if (!window.app.isLogined())
+		if (!window.app.isLoaded)
 			return [];
 
 		return window.app.images
@@ -33,7 +33,7 @@ export default window.Image = new class {
 	}
 
 	sortByNewer() {
-		if (!window.app.isLogined())
+		if (!window.app.isLoaded)
 			return [];
 
 		return window.app.images.sort((iA, iB)=> {
@@ -42,7 +42,7 @@ export default window.Image = new class {
 	}
 
 	sortByFavorites() {
-		if (!window.app.isLogined())
+		if (!window.app.isLoaded)
 			return [];
 
 		return window.app.images
@@ -54,7 +54,7 @@ export default window.Image = new class {
 	}
 
 	sortByRelatedEffort(imageID) {
-		if (!window.app.isLogined())
+		if (!window.app.isLoaded)
 			return [];
 
 		let images;
@@ -69,14 +69,14 @@ export default window.Image = new class {
 
 	// INFO: sortByRelatedアルゴリズムに必要なfavrite数があればtrue
 	isEnoughToShowRecommendation(imageID) {
-		if (!window.app.isLogined())
+		if (!window.app.isLoaded)
 			return [];
 
 		return window.app.favorites.where({ imageID }).length > 3
 	}
 
 	filterByMyFavorite() {
-		if (!window.app.isLogined())
+		if (!window.app.isLoaded)
 			return [];
 
 		return window.app.images.filter((i)=> window.app.favorites.where({ imageID: i.id, userID: window.app.session.id }).length);
@@ -84,7 +84,7 @@ export default window.Image = new class {
 
 	// TODO: Arrayをextendsしたら引数をとる
 	excludeIFavorited(images) {
-		if (!window.app.isLogined())
+		if (!window.app.isLoaded)
 			return [];
 
 		return images.filter((i)=> {
@@ -95,7 +95,7 @@ export default window.Image = new class {
 	}
 
 	isIFavorited(imageID) {
-		if (!window.app.isLogined())
+		if (!window.app.isLoaded)
 			return [];
 
 		return !!window.app.favorites.where({imageID: imageID, userID: window.app.session.id}).length;
